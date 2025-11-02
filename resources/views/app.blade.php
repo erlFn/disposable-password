77f2a3e2847d0fb142c4a05847ea7a957d8fa63d
Earl Francis Villarias <earlvillariaswork@gmail.com>
2025-11-02 23:24:37 +0800

    feat: default setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -43,7 +43,7 @@
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-geist antialiased">
         @inertia
     </body>
 </html>
